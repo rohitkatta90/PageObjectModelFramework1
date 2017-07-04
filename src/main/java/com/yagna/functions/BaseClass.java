96916b8c24eabe14946d749260971e9430458c9d
rohitkatta90 <rkvishwalaxmi@gmail.com>
2017-07-04 06:32:11 +0530

    added config.properties file and its related code

--- src/main/java/com/yagna/functions/BaseClass.java
+++ src/main/java/com/yagna/functions/BaseClass.java
@@ -1,9 +1,26 @@
 package com.yagna.functions;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 
 public class BaseClass {
 
 	public static WebDriver driver;
+	public static Properties conf=null;
+	
+	public BaseClass(){
+		
+		String sFileName = System.getProperty("user.dir")+"src\\main\\java\\com\\yagna\\utilities\\config.properties";
+		try {
+			FileInputStream fis = new FileInputStream(sFileName);
+			conf.load(fis);
+		} catch (Exception e) {
+			System.out.println("Inside BaseClass --> problem finding config file..OR loading config file...");
+			e.printStackTrace();
+		}
+		
+	}
 	
 }
